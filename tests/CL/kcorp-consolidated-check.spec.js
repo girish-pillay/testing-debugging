2049c1ed4fcb2e6106fda3b4529a093f14481721
Girish Pillay <pillay.girish@gmail.com>
2026-06-02 07:14:43 +0530

    Kcorp files modified

--- tests/CL/kcorp-consolidated-check.spec.js
+++ tests/CL/kcorp-consolidated-check.spec.js
@@ -1,6 +1,10 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../../pageObject/loginpage');
-const dataset = JSON.parse(JSON.stringify(require('../../cred/credential.json')));
+//const dataset = JSON.parse(JSON.stringify(require('../../cred/credential.json')));
+const dataset = {
+  username: process.env.CW_USERNAME,
+  password: process.env.CW_PASSWORD
+  };
 
 const EXPECTED_FILES = [
   'ANC',
